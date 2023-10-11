7edf5968f7fb9c8326ec4cd0de3873ab0e8eb414
chelfonseca <michelbeserradafonseca@gmail.com>
2023-10-11 16:57:37 -0300

    crypt password

--- src/main/java/br/com/chelsystems/todolist/user/UserController.java
+++ src/main/java/br/com/chelsystems/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,6 +26,11 @@ public class UserController {
             System.out.println("User already exists");   
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
+
+        var passwordHashed = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
