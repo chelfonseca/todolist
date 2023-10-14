1eb3427525205719e19a56109ac52961063d7401
chelfonseca <michelbeserradafonseca@gmail.com>
2023-10-14 07:03:43 -0300

    Exception handler and Dockerfile deploy

--- src/main/java/br/com/chelsystems/todolist/task/TaskModel.java
+++ src/main/java/br/com/chelsystems/todolist/task/TaskModel.java
@@ -30,5 +30,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("Title must to be 50 charactes at most");
+        }
+        this.title = title;
+    }
     
 }
